eecfe05bf73393f992a3be0ae50ca89d776abe90
maniabang <madmanno@naver.com>
2021-09-21 13:05:26 +0900

    WordRelay

---
+++ ReactZeroCho/lecture/WordRelay.jsx
@@ -0,0 +1,14 @@
+const React = require('react');
+const { Module } = require('webpack');
+
+class WordRelay extends React.Component{
+    state = {
+
+    };
+
+    render() {
+
+    }
+}
+
+module.exports = WordRelay;
---
+++ ReactZeroCho/lecture/webpack.config.js
@@ -0,0 +1,18 @@
+const path = require('path');
+
+module.exports = {
+    name: 'wordrelay-setting',
+    mode: 'development', // 실서비스: production
+    devtool: 'eval',
+    resolve: {
+        extensions: ['.js', '.jsx']
+    },
+
+    entry: {
+        app: ['./client'],
+    }, // 입력
+    output: {
+        path: path.join(__dirname, 'dist'),
+        filename: 'app.js'
+    }, // 출력
+};
