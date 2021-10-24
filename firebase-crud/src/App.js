ef49f41e52b7ce9b937daadee14550e4fd724e23
maniabang <madmanno@naver.com>
2021-10-24 13:27:16 +0900

    FirebaseCrudWeb

---
+++ firebase-crud/src/App.js
@@ -0,0 +1,14 @@
+
+import Contacts from "./components/Contacts";
+
+function App() {
+  return (
+    <div className="row"> 
+      <div className="col-md-8 offset-md-1">
+        <Contacts />
+      </div>
+    </div>
+  );
+}
+
+export default App;
